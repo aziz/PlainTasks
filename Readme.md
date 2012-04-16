035f6bd5c0380b90b550281ac687c97193d14552
Allen Bargi <allen.bargi@gmail.com>
2012-04-16 09:28:48 +0200

    First commit

---
+++ Readme.md
@@ -0,0 +1,38 @@
+# SublimeTasks
+An opinionated todo-list!
+
+## Installation
+To install this plugin, do the following:
+
+> Clone source code to Sublime Text 2 app folder
+    eg. ~/Library/Application Support/Sublime Text 2/SublimeTasks
+
+## Usage 
+`⌘ + enter` or `⌘ + i`: new task
+`⌘ + d`: toggle task completed
+`⌘ + shift + a`: archive completed tasks
+
+
+## Samples 
+Suppose we have a following todo file:
+
+	Project A:
+	☐ call mum tomorrow at 8 am.
+      ☐ talk about something.
+
+Highlight this item line and press "⌘ + d", it marks a tag "@done" and also appends timestamp.
+
+	Project A:
+	✔ call mum tomorrow at 8 am. @done (2012-01-08 18:12)
+      ✔ talk about something. @done (2012-01-08 18:13)
+
+
+## Inspiraton
+This is a fork of the original iTodo @ https://github.com/chagel/itodo
+SublimeTasks is a GTD tool for managing todo list in Sublime Text editor.
+
+Thanks to Chagel for the base plugin (https://github.com/chagel/itodo).
+Thanks to Taskmate for TextMate (https://github.com/svenfuchs/taskmate).
+Thanks to TaskPaper Mac application from hogbaysoftware.com
+
+
---
+++ sublimeTasks.py
@@ -0,0 +1,126 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import re
+import sublime
+import sublime_plugin
+from datetime import datetime
+
+
+class SublimeTasksBase(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.runCommand(edit)
+
+
+class NewCommand(SublimeTasksBase):
+    def runCommand(self, edit):
+        self.settings = sublime.load_settings("tasks.sublime-settings")
+        open_tasks_bullet = self.settings.get('open_tasks_bullet')
+        done_tasks_bullet = self.settings.get('done_tasks_bullet')
+        for region in self.view.sel():
+            line = self.view.line(region)
+            line_contents = self.view.substr(line).rstrip()
+            has_bullet = re.match('^(\s*)[' + re.escape(open_tasks_bullet) + re.escape(done_tasks_bullet) + ']', self.view.substr(line))
+            if has_bullet:
+                grps = has_bullet.groups()
+                line_contents = self.view.substr(line) + '\n' + grps[0] + open_tasks_bullet + ' '
+                self.view.replace(edit, line, line_contents)
+            else:
+                has_space = re.match('^(\s+)(.*)', self.view.substr(line))
+                # is_heading = self.view.score_selector(line, "keyword.control.header.todo") == 0
+                # print is_heading
+                if has_space:
+                    grps = has_space.groups()
+                    spaces = grps[0]
+                    line_contents = spaces + open_tasks_bullet + ' ' + grps[1]
+                    self.view.replace(edit, line, line_contents)
+                else:
+                    line_contents = ' ' + open_tasks_bullet + ' ' + self.view.substr(line)
+                    self.view.replace(edit, line, line_contents)
+                    end = self.view.sel()[0].b
+                    pt = sublime.Region(end, end)
+                    self.view.sel().clear()
+                    self.view.sel().add(pt)
+
+
+class CompleteCommand(SublimeTasksBase):
+    def runCommand(self, edit):
+        self.settings = sublime.load_settings("tasks.sublime-settings")
+
+        for region in self.view.sel():
+            line = self.view.line(region)
+            line_contents = self.view.substr(line).rstrip()
+            open_tasks_bullet = self.settings.get('open_tasks_bullet')
+            done_tasks_bullet = self.settings.get('done_tasks_bullet')
+            rom = '^(\s*)' + re.escape(open_tasks_bullet) + '\s*(.*)$'
+            rdm = '^(\s*)' + re.escape(done_tasks_bullet) + '\s*([^\b]*)\s*@done(.)+\)$'
+            open_matches = re.match(rom, line_contents)
+            done_matches = re.match(rdm, line_contents)
+            if open_matches:
+                grps = open_matches.groups()
+                self.view.insert(edit, line.end(), " @done (%s)" % datetime.now().strftime("%Y-%m-%d %H:%M"))
+                replacement = u'%s%s %s' % (grps[0], done_tasks_bullet, grps[1].rstrip())
+                self.view.replace(edit, line, replacement)
+            elif done_matches:
+                grps = done_matches.groups()
+                replacement = u'%s%s %s' % (grps[0], open_tasks_bullet, grps[1].rstrip())
+                self.view.replace(edit, line, replacement)
+
+
+class ArchiveCommand(SublimeTasksBase):
+    def runCommand(self, edit):
+        #print "========================================================================"
+
+        self.settings = sublime.load_settings("tasks.sublime-settings")
+        done_tasks_bullet = self.settings.get('done_tasks_bullet')
+        file_content = self.view.substr(sublime.Region(0, self.view.size()))
+        done_tasks = []
+        rdm = '^(\s*)' + re.escape(done_tasks_bullet) + '\s*([^\b]*)\s*@done(.)+\)$'
+        # finding done tasks
+        for line in file_content.split("\n"):
+            if line == "Archive:":
+                break
+            done_matches = re.match(rdm, line)
+            if done_matches:
+                done_tasks.append(line)
+
+        print done_tasks
+
+        # deleting done tasks
+        for task in done_tasks:
+            region = self.view.find(re.escape(task + "\n"), 0)
+            print region
+            self.view.erase(edit, region)
+
+        # finding archive section
+        archive_pos = self.view.find("Archive:", 0)
+        if archive_pos:
+            line = self.view.line(archive_pos)
+            line_content = self.view.substr(line)
+        else:
+            self.view.insert(edit, self.view.size(), "\n\n_________________________\nArchive:")
+            line = self.view.line(self.view.size())
+            line_content = self.view.substr(line)
+
+        # adding done tasks to archive section
+        self.view.replace(edit, line, line_content + "\n" + "\n".join(done_tasks))
+
+        # if no archive section add one at the end of file
+        # manipulate each done task and prefix it with project name
+
+        # x = sublime.active_window().active_view()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
