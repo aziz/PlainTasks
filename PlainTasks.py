34905c865e93cbd8d9afaa52ee046d97ce4fa907
Stanislav Parfeniuk <travmik@gmail.com>
2013-01-22 15:13:16 +0200

    Final small refactoring

--- PlainTasks.py
+++ PlainTasks.py
@@ -193,33 +193,30 @@ class OpenUrlCommand(sublime_plugin.TextCommand):
         start = s.a
         end = s.b
 
-        # if nothing is selected, expand selection to nearest terminators
-        if (start == end):
-            view_size = self.view.size()
-            terminator = ['\t', ' ', '\"', '\'','>','<',',']
+        # expand selection to nearest stopSymbols
+        view_size = self.view.size()
+        stopSymbols = ['\t', ' ', '\"', '\'','>','<',',']
+        # move the selection back to the start of the url
+        while (start > 0
+                and not self.view.substr(start - 1) in stopSymbols
+                and self.view.classify(start) & sublime.CLASS_LINE_START == 0):
+            start -= 1
 
-            # move the selection back to the start of the url
-            while (start > 0
-                    and not self.view.substr(start - 1) in terminator
-                    and self.view.classify(start) & sublime.CLASS_LINE_START == 0):
-                #print self.view.classify(start), sublime.CLASS_LINE_START
-                start -= 1
-
-            # move end of selection forward to the end of the url
-            while (end < view_size
-                    and not self.view.substr(end) in terminator
-                    and self.view.classify(end) & sublime.CLASS_LINE_END == 0):
-                end += 1
+        # move end of selection forward to the end of the url
+        while (end < view_size
+                and not self.view.substr(end) in stopSymbols
+                and self.view.classify(end) & sublime.CLASS_LINE_END == 0):
+            end += 1
 
         # grab the URL
         url = self.view.substr(sublime.Region(start, end))
-        #optional select URL
+        # optional select URL
         self.view.sel().add(sublime.Region(start, end))
 
         exp = re.search(self.URL_REGEX, url, re.X)
         if exp and exp.group(0):
             strUrl = exp.group(0)
-            if strUrl.find("http://") == -1 and strUrl.find("https://") == -1:
+            if strUrl.find("://") == -1:
                 strUrl = "http://" + strUrl
             webbrowser.open_new_tab(strUrl)
         else:
